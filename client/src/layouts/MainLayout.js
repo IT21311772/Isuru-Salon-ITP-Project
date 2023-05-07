8fa1c6fe07ff2ec6cc656e84f1660589ae19f1dd
it21308598 <it21308598@my.sliit.lk>
2023-05-08 00:01:41 +0530

    update
    new create

--- client/src/layouts/MainLayout.js
+++ client/src/layouts/MainLayout.js
@@ -26,7 +26,6 @@ function MainLayout({children}) {
           
           <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
             <label className="navbar-brand" onClick={()=>navigate("/dash")}>
-              Finance Management
             </label>
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               {/* <li className="mt-2" style={{marginLeft:'-40%'}}> 
